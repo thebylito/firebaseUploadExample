a95b84be198a3714ed39608dfe31bfa729e21cc2
Welington da Silva <thebylito@gmail.com>
2017-10-22 03:04:22 -0300

    adicionado controle de progresso

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Image, Slider, Dimensions  } from 'react-native';
+const width = Dimensions.get('screen').width;
 import ImagePicker from 'react-native-image-crop-picker';
-import RNFirebase from 'react-native-firebase';
-const firebase = RNFirebase.initializeApp();
+import firebase from 'react-native-firebase';
+
+//const firebase = RNFirebase.initializeApp();
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      downloadURL: ''
+      downloadURL: '',
+      estado : 0.0
     }
   }
 
@@ -17,26 +20,57 @@ export default class App extends Component {
     const mime = 'image/jpeg';
 
     const sessionId = new Date().getTime()
-    firebase.storage()
+  uploadImagem = firebase.storage()
       .ref('/imagems').child(`${sessionId}.jpg`)
-      .putFile(path, { contentType: mime })
-      .then(uploadedFile => {
-        console.log(uploadedFile);
-        const { downloadURL } = uploadedFile;
+      .put(path, { contentType: mime });
+
+      uploadImagem.on('state_changed', (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progresso = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        console.log(progresso + '% ');
         this.setState({
-          downloadURL
+          estado: progresso
+        },()=>{
+          console.log(snapshot.state)
+
         })
-      })
-      .catch(err => {
-        console.log(err);
-        //Error
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // ou 'paused'
+            console.log('upload pausado');
+            break;
+          case firebase.storage.TaskState.RUNNING: // ou 'running'
+            console.log('upload em andamento');
+            break;
+          }
+        }, (error) => {
+          ImagePicker.clean().then(() => {
+            console.log('arquivos temporarios removido');// para limpar o cache
+          }).catch(e => {
+            console.log(e);
+          });
+          console.log(error);
+        }, (sucess) => {
+          ImagePicker.clean().then(() => {
+            console.log('arquivos temporarios removido');// para limpar o cache
+          }).catch(e => {
+            console.log(e);
+          });
+          console.log(sucess);
+          const { downloadURL } = sucess;
+          this.setState({
+            downloadURL
+          })
+
       });
   }
 
   selecionarFoto = () => {
     ImagePicker.openPicker({
-      width: 300,
-      height: 400,
+      width: 1024,
+      height: 1024,
+      //cropperCircleOverlay: true,
       cropping: true
     }).then((image) => {
       this.uploadFoto(image);
@@ -48,7 +82,7 @@ export default class App extends Component {
   _renderFotoEnviada(){
     const { downloadURL } = this.state;
     if(downloadURL != ''){
-      return (<Image source={{uri: downloadURL}} style={{width: 300, height: 300}}/>)
+      return (<View style={{elevation: 5}}><Image source={{uri: downloadURL}} style={{width: 300, height: 400}}/></View>)
     }
     return null
   }
@@ -66,6 +100,8 @@ export default class App extends Component {
         <Button title='Selecionar uma imagem' onPress={() => {
           this.selecionarFoto()
         }} />
+        <Slider style={{width}} value={this.state.estado} minimumValue={0.0} maximumValue={100.0} step={this.state.estado}/>
+        <Text>Enviando: {Math.floor(this.state.estado) } %</Text>
       </View>
     );
   }
