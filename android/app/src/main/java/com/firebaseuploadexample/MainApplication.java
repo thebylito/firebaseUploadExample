ebbdc4d976cf859648ddef5f395991ea944fa7ad
Welington da Silva <thebylito@gmail.com>
2017-10-21 22:19:17 -0300

    instalado o firebase Storage e Image crop picker

--- android/app/src/main/java/com/firebaseuploadexample/MainApplication.java
+++ android/app/src/main/java/com/firebaseuploadexample/MainApplication.java
@@ -9,7 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // <-- Add this line
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new PickerPackage()
+            new PickerPackage(),
+            new RNFirebaseStoragePackage() // <-- Add this line
       );
     }
 
