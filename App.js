bb4554033cb678b173ea1cd7b027b34e4be8305a
Welington da Silva <thebylito@gmail.com>
2017-10-22 00:12:16 -0300

    quase final release

--- App.js
+++ App.js
@@ -1,37 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
+import RNFirebase from 'react-native-firebase';
+const firebase = RNFirebase.initializeApp();
+const availability = firebase.googleApiAvailability;
+console.log(availability);
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      downloadURL: ''
+    }
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  uploadFoto = (dados) => {
+    const { path } = dados;
+    const mime = 'image/jpeg';
 
-export default class App extends Component<{}> {
+    const sessionId = new Date().getTime()
+    firebase.storage()
+      .ref('/imagems').child(`${sessionId}.jpg`)
+      .putFile(path, { contentType: mime })
+      .then(uploadedFile => {
+        console.log(uploadedFile);
+        const { downloadURL } = uploadedFile;
+        this.setState({
+          downloadURL
+        })
+      })
+      .catch(err => {
+        console.log(err);
+        //Error
+      });
+  }
+
+  selecionarFoto = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true
+    }).then((image) => {
+      this.uploadFoto(image);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  _renderFotoEnviada(){
+    const { downloadURL } = this.state;
+    if(downloadURL != ''){
+      return (<Image source={{uri: downloadURL}} style={{width: 300, height: 300}}/>)
+    }
+    return null
+  }
+
   render() {
     return (
       <View style={styles.container}>
+      {this._renderFotoEnviada()}
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button title='Selecionar uma imagem' onPress={() => {
+          this.selecionarFoto()
+        }} />
       </View>
     );
   }
